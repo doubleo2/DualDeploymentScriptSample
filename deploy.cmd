2973d2ab2b4f24aeca58046cbe31ab585816fad3
Enrico Mills <enrico@socketmobile.com>
2017-02-17 14:52:57 -0500

    Deploy both apps simultaneously
    Configured each app with its own deployment target which corresponds with a virtual application created in the Azure portal

--- deploy.cmd
+++ deploy.cmd
@@ -1,12 +1,4 @@
 @echo off
 
-IF "%SITE_FLAVOR%" == "nodejs" (
-  deploy.node.cmd
-) ELSE (
-  IF "%SITE_FLAVOR%" == "mvc4" (
-    deploy.mvc4.cmd
-  ) ELSE (
-    echo You have to set SITE_FLAVOR setting to either "nodejs" or "mvc4"
-    exit /b 1
-  )
-)
+call deploy.node.cmd
+call deploy.mvc4.cmd
